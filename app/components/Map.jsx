a62599168faa8c1de3a32c84c12bb2e1c518df33
radu-bordea <radu.bordea.dev@gmail.com>
2025-06-01 17:08:49 +0200

    change maps api key namne

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -23,7 +23,7 @@ const Map = ({ scales }) => {
         if (!document.getElementById(scriptId)) {
           const script = document.createElement("script");
           script.id = scriptId;
-          script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`;
+          script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}`;
           script.async = true;
           script.onload = resolve; // Resolve when script is loaded
           document.body.appendChild(script);
